4e4c204a1a8b5c9f4ddf1c7a60652386058d7bcf
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2022-08-20 02:20:10 -0700

    update

--- Using_Shell_Variables.sh
+++ Using_Shell_Variables.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-# using shell variables 
 
-
+# example of nomal usage
 NAME="Han"
 AGE=26
-
 echo "$NAME is $AGE years old"
-
 readonly NAME
 unset AGE
-
 NAME="Zitong"
 echo "val AGE is $AGE"
+
+
+# special variables
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter :$2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters: $#"
+
+
+# special parameter $* and $@
+# these two parameters allow accessing all the command-line arguments at once.
+# both will act the same! unless they are enclosed in double quotes ""
+
+for TOKEN in $*
+do
+    echo $TOKEN
+done
+
+
 
 
 
