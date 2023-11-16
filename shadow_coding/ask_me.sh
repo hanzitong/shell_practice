879a448bf06f566cdc33725e14affe4372be4873
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-11-16 19:38:16 +0900

    micro commit on ask_me.sh

--- shadow_coding/ask_me.sh
+++ shadow_coding/ask_me.sh
@@ -2,13 +2,13 @@
 
 # practice variables and echo, read sentence
 
-# vim memo >> esc = Ctrl+[
 
 echo "Input your name plz"
 read NAME
-read -p "Input your age plz" AGE
+read -p "Input your age plz: " AGE
 
 echo "hello, $NAME $AGE years old"
+echo 'hello, $NAME $AGE years old'
 
 
 test
